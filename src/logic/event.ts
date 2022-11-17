a105dcd704920ded8caf23d62c5ec003ab874eb0
Jozsef Varga <joshanoid@gmail.com>
2022-11-17 16:23:26 +0100

    moved measurement types out

--- src/logic/event.ts
+++ src/logic/event.ts
@@ -1,16 +1,5 @@
-export const BROKER_SEARCH_LIST_ID = "BROKER_SEARCH_LIST_ID"
-export const TOP_5_STOCK_TAB_LIST_ID = "TOP_5_STOCK_TAB_LIST_ID"
-export const TOP_5_FOREX_TAB_LIST_ID = "TOP_5_FOREX_TAB_LIST_ID"
+import { MeasurementEvent } from "../utils/types";
 
-export type MeasurementId =
-    | typeof BROKER_SEARCH_LIST_ID
-    | typeof TOP_5_STOCK_TAB_LIST_ID
-    | typeof TOP_5_FOREX_TAB_LIST_ID
-
-export const sendEvent = (arg: {
-    type: "click" | "impression",
-    brokerId: number,
-    measurementId: MeasurementId
-}) => {
-    console.log("Event sent: ", arg)
-}
+export const sendEvent = (arg: MeasurementEvent) => {
+    console.log("Event sent: ", arg);
+};
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
+import { BROKER_SEARCH_LIST_ID, TOP_5_STOCK_TAB_LIST_ID, TOP_5_FOREX_TAB_LIST_ID } from "./constants";
+
 export type Broker = {
     id: number;
     name: string;
@@ -12,6 +14,22 @@ export type Brokers = ReadonlyArray<Broker>;
 
 export type BrokerType = "stock" | "forex";
 
+export type MeasurementId =
+    | typeof BROKER_SEARCH_LIST_ID
+    | typeof TOP_5_STOCK_TAB_LIST_ID
+    | typeof TOP_5_FOREX_TAB_LIST_ID;
+
+export type MeasurementType = "click" | "impression";
+
+export type MeasurementEvent = {
+    type: MeasurementType;
+    brokerId: number;
+    measurementId: MeasurementId;
+};
+
+export type Measurements = ReadonlyArray<MeasurementEvent>;
+
 export type RootState = {
     brokerData: Brokers;
+    measurements: Measurements;
 };

2a80cfb320e6ae456114a11cc4e02273b69d05a3
Jozsef Varga <joshanoid@gmail.com>
2022-11-17 21:13:06 +0100

    moved styles from css into component with tailwind classes

--- src/features/Dashboard/index.tsx
+++ src/features/Dashboard/index.tsx
@@ -1,13 +1,15 @@
 import { BrokerList } from "../BrokerList";
+import { Measurements } from "../Measurements";
 import { TopBrokers } from "../TopBrokers";
 import styles from "./Dashboard.module.css";
 
 export const Dashboard = () => (
-    <div className={`${styles.dashboard} pt-12 px-10`}>
+    <div className="pt-12 px-10 min-h-screen bg-sky-100">
         <h1 className="text-3xl font-bold text-center mb-6">Find the right broker and invest on your own</h1>
         <div className="flex flex-row justify-between sm:flex-col-reverse sm:items-center">
             <BrokerList />
             <TopBrokers />
         </div>
+        <Measurements />
     </div>
 );

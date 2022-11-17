e9ed734a534052848d7add14137cd5bb34d7b8f6
Jozsef Varga <joshanoid@gmail.com>
2022-11-17 15:15:41 +0100

    added clear icon to the search input

--- src/features/BrokerList/index.tsx
+++ src/features/BrokerList/index.tsx
@@ -7,19 +7,36 @@ export const BrokerList = () => {
     const debouncedSearchTerm = useDebounce(searchTerm);
 
     return (
-        <section className="flex flex-col justify-between flex-1">
-            <div className="flex flex-wrap items-center w-1/2 mb-6">
+        <section className="flex flex-col flex-1">
+            <div className="flex flex-col w-1/2 mb-6">
                 <label htmlFor="broker-filter" className="block text-sm ml-2 mb-1">
                     Filter by name
                 </label>
-                <input
-                    className="block py-2 px-2 w-full text-sm font-medium outline-none bg-white border border-gray-400 rounded-md"
-                    id="broker-filter"
-                    type="text"
-                    placeholder="Type Broker Name"
-                    value={searchTerm}
-                    onChange={(event) => setSearchTerm(event.target.value)}
-                />
+                <div className="relative">
+                    <input
+                        className="block py-2 px-2 w-full text-sm font-medium outline-none bg-white border border-gray-400 rounded-md"
+                        id="broker-filter"
+                        type="text"
+                        placeholder="Type Broker Name"
+                        value={searchTerm}
+                        onChange={(event) => setSearchTerm(event.target.value)}
+                    />
+                    <svg
+                        className="w-4 h-4 absolute inset-y-0 my-auto right-0 mr-2 cursor-pointer"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        onClick={() => setSearchTerm("")}
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M6 18L18 6M6 6l12 12"
+                        ></path>
+                    </svg>
+                </div>
             </div>
             <List searchTerm={debouncedSearchTerm} />
         </section>

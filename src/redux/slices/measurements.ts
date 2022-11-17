ab668057df84c710a75c3a83f873dd9ccdc84de9
Jozsef Varga <joshanoid@gmail.com>
2022-11-17 19:04:52 +0100

    added async thunk addMeasurement for handling sendEvent side effect in a redux action

--- src/redux/slices/measurements.ts
+++ src/redux/slices/measurements.ts
@@ -1,7 +1,26 @@
-import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { sendEvent } from "../../logic/event";
 import { MeasurementEvent, Measurements, RootState } from "../../utils/types";
 
 const initialState: Measurements = [];
+
+export const addMeasurement = createAsyncThunk("addMeasurement", async (measurement: MeasurementEvent, thunkAPI) => {
+    const state = thunkAPI.getState() as RootState;
+
+    if (
+        !state.measurements.find(
+            (storedMeasurement) =>
+                storedMeasurement.brokerId === measurement.brokerId &&
+                storedMeasurement.measurementId === measurement.measurementId &&
+                measurement.type === "impression"
+        )
+    ) {
+        sendEvent(measurement);
+        return measurement;
+    }
+
+    throw new Error("Impression event is already in the store");
+});
 
 export const selectMeasurements = createSelector(
     (state: RootState) => state.measurements,
@@ -11,7 +30,10 @@ export const selectMeasurements = createSelector(
 export const measurementsSlice = createSlice({
     name: "measurements",
     initialState,
-    reducers: {
-        addMeasurement: (state: Measurements, action: PayloadAction<MeasurementEvent>) => [...state, action.payload]
+    reducers: {},
+    extraReducers: {
+        [addMeasurement.fulfilled.type]: (state: Measurements, action: PayloadAction<MeasurementEvent>) => {
+            return [...state, action.payload];
+        }
     }
 });

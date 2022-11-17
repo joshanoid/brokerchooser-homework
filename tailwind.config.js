2a7350b2f513802076183372ee2f156f208144ff
Jozsef Varga <joshanoid@gmail.com>
2022-11-17 20:42:13 +0100

    overridden the default behaviour of tailwind responsive system, applied max-width to sm breakpoint instead

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+        screens: {
+            sm: { max: "640px" }
+        }
+    },
+    plugins: []
+};
